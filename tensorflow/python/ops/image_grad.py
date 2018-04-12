d858000117f4a73c4c1ef5b471d44e48b71200e8
WKChung1028 <32151938+WKChung1028@users.noreply.github.com>
2018-04-12 18:03:06 +0800

    Update image_grad.py

--- tensorflow/python/ops/image_grad.py
+++ tensorflow/python/ops/image_grad.py
@@ -120,3 +120,42 @@ def _CropAndResizeGrad(op, grad):
                                                    op.inputs[1], op.inputs[2])
 
   return [grad0, grad1, None, None]
+
+@ops.RegisterGradient("CropAndResizeQuad")
+def _CropAndResizeQuadGrad(op, grad):
+  """The derivatives for crop_and_resize.
+
+  We back-propagate to the image only when the input image tensor has floating
+  point dtype but we always back-propagate to the input boxes tensor.
+  Mainly same as  CropAndResize.
+
+  Args:
+    op: The CropAndResizeQuad op.
+    grad: The tensor representing the gradient w.r.t. the output.
+
+  Returns:
+    The gradients w.r.t. the input image, boxes, as well as the always-None
+    gradients w.r.t. box_ind and crop_size.
+  """
+  image = op.inputs[0]
+  if image.get_shape().is_fully_defined():
+    image_shape = image.get_shape().as_list()
+  else:
+    image_shape = array_ops.shape(image)
+
+  allowed_types = [dtypes.float16, dtypes.float32, dtypes.float64]
+  if op.inputs[0].dtype in allowed_types:
+    # pylint: disable=protected-access
+    grad0 = gen_image_ops.crop_and_resize_quad_grad_image(grad,
+                                                     op.inputs[1],
+                                                     op.inputs[2],
+                                                     image_shape,
+                                                     T=op.get_attr("T"))
+    # pylint: enable=protected-access
+  else:
+    grad0 = None
+
+  grad1 = gen_image_ops.crop_and_resize_quad_grad_boxes(grad, op.inputs[0],
+                                                   op.inputs[1], op.inputs[2])
+
+  return [grad0, grad1, None, None]
